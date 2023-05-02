c3628d47ed774c4fd03225c6a9d565bec2c42a47
Alvian Ghifari <alvian2002286@gmail.com>
2023-05-02 22:17:41 +0700

    feat: create task entity

---
+++ entity/task.go
@@ -0,0 +1,14 @@
+package entity
+
+import "time"
+
+type Task struct {
+	ID          uint   `gorm:"primaryKey"`
+	Title       string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Status      bool   `gorm:"not null;default:false"`
+	UserID      uint
+	CategoryID  uint
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
