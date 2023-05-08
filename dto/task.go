6b98c6b4f64dea20d7a06a0b1aea22c1fe6f2b52
Alvian Ghifari <alvian2002286@gmail.com>
2023-05-08 21:31:57 +0700

    feat: edit task dtos

--- dto/task.go
+++ dto/task.go
@@ -17,3 +17,16 @@ type CreateTaskResponse struct {
 	CategoryID  uint      `json:"category_id"`
 	CreatedAt   time.Time `json:"created_at"`
 }
+type EditTaskRequest struct {
+	Title       string `json:"title" valid:"required~title cannot be empty"`
+	Description string `json:"description" valid:"required~description cannot be empty"`
+}
+type EditTaskResponse struct {
+	ID          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      bool      `json:"status"`
+	UserID      uint      `json:"user_id"`
+	CategoryID  uint      `json:"category_id"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
