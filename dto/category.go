ec30ed5d58c4c43c6800f71e2debd806e4c20375
Alvian Ghifari <alvian2002286@gmail.com>
2023-05-08 23:28:27 +0700

    feat: get category dtos

--- dto/category.go
+++ dto/category.go
@@ -1,6 +1,8 @@
 package dto
 
-import "time"
+import (
+	"time"
+)
 
 type CategoryRequest struct {
 	Type string `json:"type" valid:"required~type cannot be empty"`
@@ -15,4 +17,22 @@ type UpdateCategoryResponse struct {
 	Id        uint      `json:"id"`
 	Type      string    `json:"type"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type GetCategoryResponse struct {
+	Id        uint           `json:"id"`
+	Type      string         `json:"type"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	Tasks     []CategoryTask `json:"Tasks"`
+}
+
+type CategoryTask struct {
+	ID          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	UserID      uint      `json:"user_id"`
+	CategoryID  uint      `json:"category_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
